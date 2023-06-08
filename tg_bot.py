058250a9003d62a41edd46ea92fc7423c1a50f58
miashikio <miashikio@gmail.com>
2023-06-08 13:13:46 +0300

    Добавила комментраии и завершила проект

--- tg_bot.py
+++ tg_bot.py
@@ -4,8 +4,11 @@ TOKEN = '6004830698:AAFlQT2D0UM9q7eLo-nofFU-zjebZhDFAk0'
 
 # создание экземпляра бота
 bot = telebot.TeleBot(TOKEN)
+#срабатывает по комманде /start
 @bot.message_handler(commands=['start'])
+#функция приветствия
 def start_message(message):
+#выводим сообщение приветствия
   bot.send_message(message.chat.id,"Привет ✌️ ")
 
 @bot.message_handler()
@@ -16,4 +19,5 @@ def answer(message):
     bot.send_message(message.chat.id, random.choice(answers))
 
 if __name__ == '__main__':
-  bot.infinity_polling(none_stop=True,interval=0)
+  #запуск бота
+bot.infinity_polling(none_stop=True,interval=0)
